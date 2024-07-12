fc6f28c4226d00725075e6e0aaf4f7726e911f60
babinicz <babinicz@onet.pl>
2024-07-12 20:24:31 +0200

    npm i react-cookie-consent

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Footer from "./components/Footer";
 import { useTheme, ThemeProvider } from "./ThemeContext";
 import GlobalStyle from "./GlobalStyle";
 import ScrollToTopButton from "./components/ScrollToTopButton";
+import CookieConsent from "react-cookie-consent";
 
 const lightTheme = {
   background: "#ffffff",
@@ -43,6 +44,33 @@ const AppContent = () => {
         <Upamietnienie />
         <Footer />
       </AppContainer>
+      <CookieConsent
+        debug={true}
+        location="bottom"
+        style={{
+          background: "#333",
+          textAlign: "left",
+          paddingBottom: "1rem",
+          fontSize: "16px",
+          fontFamily: "Gideon Roman",
+        }}
+        buttonStyle={{
+          color: "#333",
+          background: "#fff",
+          fontSize: "18px",
+          fontFamily: "Gideon Roman",
+          marginRight: "1rem",
+        }}
+        buttonText="OK, rozumiem"
+        expires={365}
+      >
+        "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+        Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+        indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+        dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+        urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+        ustawień dotyczących cookies."
+      </CookieConsent>
     </StyledThemeProvider>
   );
 };
